5360ac036ea2339aac76c3fabea0123a6719070a
Oliwia <Oliwia@MacBook-Air-Oliwia.local>
2017-10-14 15:42:18 +0200

    first task

---
+++ js/app.js
@@ -0,0 +1,24 @@
+
+    'use strict';
+
+
+    document.addEventListener('DOMContentLoaded', function() {
+
+    	var forDropdown = document.querySelector('.for-dropdown');
+    	var dropdown = document.querySelector('.dropdown');
+
+
+    	forDropdown.addEventListener('mouseover', function() {
+    		dropdown.style.display = 'block';
+	    	});
+
+    	forDropdown.addEventListener('mouseout', function() {
+    		dropdown.style.display = 'none';
+    	});
+
+    	
+    		
+
+
+
+    });
